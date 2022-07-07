f612f8eab0ee88c48c3a8807c603f80bed3f2c31
swathi-mrk <108858410+swathi-mrk@users.noreply.github.com>
2022-07-07 19:47:00 +0530

    si & ci

--- july.js
+++ july.js
@@ -3,6 +3,12 @@ function calculatorabc1(){
 	const a = document.getElementById('text1').value;
 	const b = document.getElementById('text2').value;
 	const c = document.getElementById('text3').value;
+	let x = document.getElementById('text3').value;
+	try{
+		if(x== "") throw "empty";
+	}catch(err){
+           console.log('Please Enter Rate of Interest');
+    }		   
 	const sum = parseInt(a)* parseInt(b)* parseInt(c)/100;
 	document.getElementById('text4').value = sum;
 }
